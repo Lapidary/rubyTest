e56c6cf0b6016b1527cec89803e22a7fd48efbbb
Lapidary <xy3839401@163.com>
2017-12-21 13:08:22 +0000

    s

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -84,7 +84,7 @@ class BookInStock
 
     def price_as_stringdasda
         return "$#{'%.2f' % price}"sdfdfsdvsdfcacdscdscdsc
-    vfdvdfvdvcsdcdscdc
+    vfdvdfvdvcsdcdscdcefrefe
     attr_accessor :isbncdcdscdshff
     attr_accessor :pricevdvfdvdfvf
 cdvs
