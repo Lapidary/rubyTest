a439c7c3356440f1c9f34a2247e6bf0ed531fb99
Lapidary <xy3839401@163.com>
2017-12-21 10:16:08 +0000

    s

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -83,7 +83,7 @@ class BookInStock
     # enddasfsfwefewf
 
     def price_as_stringdasda
-        return "$#{'%.2f' % price}"sdfdfsdvsdfca
+        return "$#{'%.2f' % price}"sdfdfsdvsdfcacdscdscdsc
     vfdvdfvdv
     attr_accessor :isbncdcdscds
     attr_accessor :price
